7d45032039510db0362cf3fbd168b75df9c0ed1a
liechtym <matthew@liechty.org>
2016-08-24 14:20:10 -0600

    Adding mobile handling

---
+++ wct.conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "plugins": {
+    "istanbul": {
+      "dir": "./coverage",
+      "reporters": [
+        "text-summary", "lcov"
+      ],
+      "exclude": [],
+
+      "include": [
+        "**/birch-contact-card.html"
+      ]
+    },
+    "local": {
+      "browsers": ["chrome", "safari"]
+    }
+  }
+}
